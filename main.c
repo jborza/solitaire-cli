50e67625304248ae109457ad59b0e96814bf9936
Juraj Borza <jborza@gmail.com>
2020-07-02 17:19:21 +0200

    extracted test code

--- main.c
+++ main.c
@@ -346,37 +346,6 @@ game_state *make_game_state() {
   return state;
 }
 
-void test_cards() {
-  card c5H = make_card(SUIT_HEART, RANK_5);
-  card c5S = make_card(SUIT_SPADE, RANK_5);
-  card c6H = make_card(SUIT_HEART, RANK_6);
-  card c6D = make_card(SUIT_DIAMOND, RANK_6);
-  card c5D = make_card(SUIT_DIAMOND, RANK_5);
-  card c7D = make_card(SUIT_DIAMOND, RANK_7);
-  printf("5s is black %d vs 1 \n", is_black(c5S));
-  printf("5s is red %d vs 0 \n", is_red(c5S));
-  printf("6h is red %d vs 1 \n", is_red(c6H));
-  printf("6h is black %d vs 0 \n", is_black(c6H));
-  printf("5h 6h is alternate %d vs 0 \n", is_alternate_color(c5H, c6H));
-  printf("5s 6h is alternate %d vs 1 \n", is_alternate_color(c5S, c6H));
-  printf("5d 6d is same suit %d vs 1 \n", is_same_suit(c5D, c6D));
-  printf("6h 6d is same suit %d vs 0 \n", is_same_suit(c6H, c6D));
-  printf("5s 6h in sequence %d vs 1 \n", is_in_sequence(c5S, c6H));
-  printf("6h 6d in sequence %d vs 0 \n", is_in_sequence(c6H, c6D));
-  printf("5s 7d in sequence %d vs 0 \n", is_in_sequence(c5S, c7D));
-  printf("5h 6h can be placed on foundation %d vs 1\n",
-         can_be_placed_on_foundation(c5H, c6H));
-  printf("5h 6h can be placed on foundation %d vs 0\n",
-         can_be_placed_on_foundation(c6H, c5H));
-  printf("6h 5s can be placed on the bottom %d vs 1\n",
-         can_be_placed_bottom(c6H, c5S));
-  printf("5h 6h can be placed on the bottom %d vs 0\n",
-         can_be_placed_bottom(c5H, c6H));
-  printf("6h 5h can be placed on the bottom %d vs 0\n",
-         can_be_placed_bottom(c6H, c5H));
-  // TODO can_be_placed_on_bottom test
-}
-
 void print_deck(pile *pile) {
   printf("[");
   card_node *head = pile->head;
@@ -396,11 +365,6 @@ void print_pile_ptrs(pile *pile) {
     card *c = head->value;
     print_card(c);
     printf(" @ %p | ", c);
-    // second row peek
-    // for (int i = 0; i < COLUMN_COUNT; i++) {
-    //  move(6, column_size * i);
-    //  printw_card(peek(column(state, i)));
-    //}
 
     head = head->next;
   }
@@ -433,51 +397,6 @@ void shuffle_pile(pile *pile) {
   print_card_ptr(x);                                                           \
   printf("\n");
 
-void test_pile_operations() {
-  pile *initial_deck = make_pile();
-  fill_deck(initial_deck);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\npop\n");
-  card_ptr popped_card = pop(initial_deck);
-  PRINTCARD(popped_card);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\npop\n");
-  card_ptr popped_card_2 = pop(initial_deck);
-  PRINTCARD(popped_card_2);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\nshift\n");
-  card_ptr dequeued_card = shift(initial_deck);
-  PRINTCARD(dequeued_card);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\nshift\n");
-  card_ptr dequeued_card_2 = shift(initial_deck);
-  PRINTCARD(dequeued_card_2);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\nunshift 2H\n");
-  unshift(initial_deck, dequeued_card_2);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\nunshift 3H\n");
-  unshift(initial_deck, dequeued_card);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-
-  printf("\npush 4H\n");
-  push(initial_deck, popped_card_2);
-  print_deck(initial_deck);
-  print_pile_ptrs(initial_deck);
-}
 
 pile *stock(game_state *state) { return state->piles[PILE_DECK]; }
 
